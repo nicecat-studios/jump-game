804f53c0b65f5b01140956ffa1d6e69f382e1e3a
Christer Warén <cwchristerw@gmail.com>
2020-02-21 04:29:40 +0200

    Pressing NiceCat logo now will open website in a browser, also added two other links already ahead in OpenLink script.

---
+++ Jump Game/Assets/Scripts/Menu/OpenLink.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void Website(){	
+        Application.OpenURL("https://nicecat.wgi.fi");
+    }
+    
+    public void Website_GamePage(){
+        Application.OpenURL("https://nicecat.wgi.fi/jump-game/");
+    }
+    
+    public void GitRepositories_Github(){
+        Application.OpenURL("https://github.com/nicecat-studios/jump-game");
+    }
+    
+    public void GitRepositories_Gitlab(){
+        Application.OpenURL("https://gitlab.com/nicecat-studios/jump-game");
+    }
+    
+}
