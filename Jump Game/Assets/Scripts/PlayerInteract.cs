78ed8d1285da62968b731f9ff1734bec955a5d0c
BuildTools <unconfigured@null.spigotmc.org>
2020-04-10 08:45:04 +0300

    npc talk to nearby players

---
+++ Jump Game/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public GameObject currentInterObj = null;
+    public InteractionObject currentInterObjScript = null;
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown ("Interact") && currentInterObj)
+        {
+            if (currentInterObjScript.talks)
+            {
+                currentInterObjScript.Talk();
+            }
+
+        }
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("interObject"))
+        {
+            Debug.Log(other.name);
+            currentInterObj = other.gameObject;
+            currentInterObjScript = currentInterObj.GetComponent <InteractionObject>  ();
+        }
+    }
+}
