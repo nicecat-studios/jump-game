1ab47242259d8f015ef038dbd611abea9791babe
Christer Warén <cwchristerw@gmail.com>
2022-03-30 19:38:03 +0300

    Update

--- Jump Game/Assets/Scripts/Menu/Menu_Buttons.cs
+++ Jump Game/Assets/Scripts/Menu/Menu_Buttons.cs
@@ -7,59 +7,23 @@ using UnityEngine.SceneManagement;
 public class Menu_Buttons : MonoBehaviour
 {
     public GameObject MainMenuPanel;
-    public GameObject OptionMenuPanel;
-    public GameObject OptionAudioPanel;
-    public GameObject OptionVideoPanel;
-    public GameObject OptionControlsPanel;
+    public GameObject SettingsMenuPanel;
 
     // Start is called before the first frame update
     void Start()
     {
         MainMenuPanel.SetActive(true);
-        OptionMenuPanel.SetActive(false);
-        OptionAudioPanel.SetActive(false);
-        OptionVideoPanel.SetActive(false);
-        OptionControlsPanel.SetActive(false);
+        SettingsMenuPanel.SetActive(false);
     }
     public void ShowMainMenuPanel()
     {
         MainMenuPanel.SetActive(true);
-        OptionMenuPanel.SetActive(false);
-        OptionAudioPanel.SetActive(false);
-        OptionVideoPanel.SetActive(false);
-        OptionControlsPanel.SetActive(false);
+        SettingsMenuPanel.SetActive(false);
     }
-    public void ShowOptionMenuPanel()
+    public void ShowSettingsMenuPanel()
     {
         MainMenuPanel.SetActive(false);
-        OptionMenuPanel.SetActive(true);
-        OptionAudioPanel.SetActive(false);
-        OptionVideoPanel.SetActive(false);
-        OptionControlsPanel.SetActive(false);
-    }
-    public void ShowOptionAudio()
-    {
-        MainMenuPanel.SetActive(false);
-        OptionMenuPanel.SetActive(true);
-        OptionAudioPanel.SetActive(true);
-        OptionVideoPanel.SetActive(false);
-        OptionControlsPanel.SetActive(false);
-    }
-    public void ShowOptionVideo()
-    {
-        MainMenuPanel.SetActive(false);
-        OptionMenuPanel.SetActive(true);
-        OptionAudioPanel.SetActive(false);
-        OptionVideoPanel.SetActive(true);
-        OptionControlsPanel.SetActive(false);
-    }
-    public void ShowOptionControls()
-    {
-        MainMenuPanel.SetActive(false);
-        OptionMenuPanel.SetActive(true);
-        OptionAudioPanel.SetActive(false);
-        OptionVideoPanel.SetActive(false);
-        OptionControlsPanel.SetActive(true);
+        SettingsMenuPanel.SetActive(true);
     }
     public void StartGame()
     {
